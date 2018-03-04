66c8732b1821a52efa9da4dee73758ee8f4cfd9d
Milos Vasic <milos85vasic@gmail.com>
2018-03-04 10:59:31 +0100

    Building WAR.

--- Journaler API/api/src/main/kotlin/com/journaler/api/ApiApplication.kt
+++ Journaler API/api/src/main/kotlin/com/journaler/api/ApiApplication.kt
@@ -2,10 +2,20 @@ package com.journaler.api
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.boot.builder.SpringApplicationBuilder
+
+
 
 @SpringBootApplication
-class ApiApplication
+class ApiApplication : SpringBootServletInitializer() {
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(ApiApplication::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(ApiApplication::class.java, *args)
 }
+
+
