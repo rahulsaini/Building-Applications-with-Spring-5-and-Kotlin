b335061d21315f0977f793bfc4c85ee96fc42f29
Milos Vasic <milos85vasic@gmail.com>
2018-05-09 20:06:30 +0200

    Fixing and polishing.

--- Journaler API/api/src/main/kotlin/com/journaler/api/reactor/TodosCountNotificationConsumer.kt
+++ Journaler API/api/src/main/kotlin/com/journaler/api/reactor/TodosCountNotificationConsumer.kt
@@ -8,7 +8,7 @@ import reactor.bus.Event
 class TodosCountNotificationConsumer : NotificationConsumer<TodosCountNotification> {
 
     @Autowired
-    private lateinit var service: TodosCountNotificationServiceImpl
+    lateinit var service: TodosCountNotificationService
 
     override fun accept(e: Event<TodosCountNotification>?) {
         val data = e?.data
